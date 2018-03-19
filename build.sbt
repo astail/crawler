c3fb7ff70ca95b80d85b0e1bfb5a37cc5a134f7c
astel <kiyomillefeuille@gmail.com>
2018-03-19 20:41:43 +0900

    設定したurlの画像をローカルに保存する

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
 organization := "net.astail"
 name := "crawler"
 version := "0.1.0-SNAPSHOT"
-scalaVersion := "2.12.4"
+scalaVersion := "2.11.8"
--- src/main/scala/net/astail/crawler/Main.scala
+++ src/main/scala/net/astail/crawler/Main.scala
@@ -1,7 +1,21 @@
 package net.astail.crawler
 
+import java.io.File
+import java.net.URL
+import javax.imageio.ImageIO
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("test")
+    val url = "https://astail.net/wp-content/uploads/2017/07/IMG_0909-e1499686509121.jpg"
+    val name = "img.jpg"
+    val dir = "/tmp/"
+    download(url, name, dir)
+  }
+
+  def download(url: String, name: String, dir: String ) = {
+    val f = new File(dir + name)
+    val u = new URL(url)
+    val r = ImageIO.read(u)
+    ImageIO.write(r,"jpg",f)
   }
 }
