b82de28ae0d0481032103bc6d2e33328e69ea449
astel <kiyomillefeuille@gmail.com>
2018-03-20 14:20:58 +0900

    表示されているjpgをDLする

--- build.sbt
+++ build.sbt
@@ -2,3 +2,5 @@ organization := "net.astail"
 name := "crawler"
 version := "0.1.0-SNAPSHOT"
 scalaVersion := "2.11.8"
+
+libraryDependencies += "org.jsoup" % "jsoup" % "1.11.2"
--- src/main/scala/net/astail/crawler/Main.scala
+++ src/main/scala/net/astail/crawler/Main.scala
@@ -4,18 +4,28 @@ import java.io.File
 import java.net.URL
 import javax.imageio.ImageIO
 
+import collection.JavaConverters._
+import org.jsoup._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    val url = "https://astail.net/wp-content/uploads/2017/07/IMG_0909-e1499686509121.jpg"
-    val name = "img.jpg"
-    val dir = "/tmp/"
-    download(url, name, dir)
+    val doc = Jsoup.connect("http://satlog.blog119.fc2.com/blog-entry-2943.html").get
+
+    for (t <- doc.select("img").asScala) {
+      val x = t.attr("src")
+      if (x.split('.').last == "jpg") {
+        val url = x
+        val name = x.split('/').last
+        val dir = "/tmp/astel/"
+        download(url, name, dir)
+      }
+    }
   }
 
-  def download(url: String, name: String, dir: String ) = {
+  def download(url: String, name: String, dir: String) = {
     val f = new File(dir + name)
     val u = new URL(url)
     val r = ImageIO.read(u)
-    ImageIO.write(r,"jpg",f)
+    ImageIO.write(r, "jpg", f)
   }
 }
