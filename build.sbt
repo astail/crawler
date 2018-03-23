c3b3b45c4afb422fad4dfd6f1f569baab997d74e
astel <kiyomillefeuille@gmail.com>
2018-03-23 19:46:07 +0900

    skinny scalaiosource

--- build.sbt
+++ build.sbt
@@ -3,4 +3,7 @@ name := "crawler"
 version := "0.1.0-SNAPSHOT"
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.jsoup" % "jsoup" % "1.11.2"
+libraryDependencies ++= Seq(
+  "org.jsoup" % "jsoup" % "1.11.2",
+  "org.skinny-framework" %% "skinny-framework" % "2.5.2"
+)
--- src/main/scala/net/astail/crawler/Main.scala
+++ src/main/scala/net/astail/crawler/Main.scala
@@ -3,25 +3,15 @@ package net.astail.crawler
 import java.io.File
 import java.net.URL
 import javax.imageio.ImageIO
-
 import collection.JavaConverters._
-import org.jsoup._
-
 import scala.util.control.NonFatal
+import org.jsoup._
+import skinny.http._
+
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val doc = Jsoup.connect("http://satlog.blog119.fc2.com/blog-entry-2943.html").get
-
-    for (t <- doc.select("img").asScala) {
-      val x = t.attr("src")
-      if (x.split('.').last == "jpg") {
-        val url = x
-        val name = x.split('/').last
-        val dir = "/tmp/astel/"
-        download(url, name, dir)
-      }
-    }
+    jsoup
   }
 
   def download(url: String, name: String, dir: String) = {
@@ -33,5 +23,34 @@ object Main {
     } catch {
       case NonFatal(t) => println(t)
     }
+  }
+
+  def jsoup = {
+    val result = Jsoup.connect("http://satlog.blog119.fc2.com/blog-entry-2943.html").get
+//    println(result)
+
+    for (t <- result.select("img").asScala) {
+      val x = t.attr("src")
+      if (x.split('.').last == "jpg") {
+        val url = x
+        val name = x.split('/').last
+        val dir = "/tmp/astel/"
+//        println(x)
+        download(url, name, dir)
+      }
+    }
+  }
+
+  def skinny = {
+    val response: Response = HTTP.get("http://satlog.blog119.fc2.com/blog-entry-2943.html")
+    print(response.textBody)
+//    if(response.textBody.contains("img")) println(response.textBody)
+  }
+
+
+  def scalaiosource: Unit = {
+    val url = "http://satlog.blog119.fc2.com/blog-entry-2943.html"
+    val result = scala.io.Source.fromURL(url).mkString
+//    println(result contains "img")
   }
 }
