20d097386e3b67bf8840acacd673ecf7153fa236
astel <kiyomillefeuille@gmail.com>
2018-05-02 20:17:21 +0900

    import 漏れ

--- src/main/scala/net/astail/crawler/Crawler3t.scala
+++ src/main/scala/net/astail/crawler/Crawler3t.scala
@@ -2,23 +2,28 @@ package net.astail.crawler
 
 import com.danielasfregola.twitter4s.TwitterRestClient
 
+
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 import scala.concurrent.duration.Duration
 import scala.util.{ Success, Failure }
 
 object Crawler3t {
-  val COUNT = 10
-  val USER = "astel4696"
-  val client = TwitterRestClient()
-  val timeLine = client.userTimelineForUser(USER, count = COUNT)
 
-  timeLine.onComplete {
-    case Success(msg) => for (tweet <- msg.data) {
-      println(s"${tweet.id}, ${tweet.text}")
+  def astelTenGet = {
+    val COUNT = 10
+    val USER = "astel4696"
+    val client = TwitterRestClient()
+    val timeLine = client.userTimelineForUser(USER, count = COUNT)
+
+    timeLine.onComplete {
+      case Success(msg) => for (tweet <- msg.data) {
+        println(s"${tweet.id}, ${tweet.text}")
+      }
+      case Failure(t) => println(t.getMessage())
     }
-    case Failure(t) => println(t.getMessage())
-  }
 
-  Await.ready(timeLine,Duration.Inf)
-  client.shutdown()
+    Await.ready(timeLine, Duration.Inf)
+    client.shutdown()
+  }
 }
