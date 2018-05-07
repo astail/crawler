35266b63cf1573732b362579727de740830e9def
astel <kiyomillefeuille@gmail.com>
2018-05-07 20:51:10 +0900

    文字列だけをとれるように

--- src/main/scala/net/astail/crawler/SearchTweet.scala
+++ src/main/scala/net/astail/crawler/SearchTweet.scala
@@ -5,15 +5,22 @@ import com.danielasfregola.twitter4s.http.clients.rest.search
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 object SearchTweet {
 
   def search = {
     val client = TwitterRestClient()
-    val searchTw = client.searchTweet("test")
+    val searchTw = client.searchTweet("HHKB lang:ja", count = 3)
+
+    searchTw.onComplete {
+      case Success(msg) => println(s"${msg.data.statuses(0).text}")
+      case Failure(t) => println(t.getMessage())
+    }
 
     Await.ready(searchTw, Duration.Inf)
-    println(searchTw)
     client.shutdown()
   }
 }
