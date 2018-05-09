9f4af300ada53cd7185df300cd3df9f16a5fcebe
astel <kiyomillefeuille@gmail.com>
2018-05-09 15:59:37 +0900

    画像のurlも取ってこれるようになった

--- src/main/scala/net/astail/crawler/SearchTweet.scala
+++ src/main/scala/net/astail/crawler/SearchTweet.scala
@@ -12,11 +12,25 @@ object SearchTweet {
 
   def search = {
     val client = TwitterRestClient()
-    val searchTw = client.searchTweet("HHKB lang:ja", count = 10)
+    val searchTw = client.searchTweet("モンエナてすとほげ", count = 10)
+
 
     searchTw.onComplete {
       case Success(msg) => for (tweet <- msg.data.statuses) {
-        println(s"${tweet.id}, ${tweet.user.get.name}, @${tweet.user.get.screen_name}, ${tweet.text}")
+
+        //println(s"${tweet.entities}")
+        //println(s"${tweet.extended_entities}")
+        //tweet.entities.get.media.foreach(x => println(x.media_url))
+
+        //画像urlだけを取ってくる
+        //tweet.extended_entities.get.media.foreach(x => println(x.media_url))
+
+        val imageGet = tweet.extended_entities.get.media.map(_.media_url)
+
+        ////println(s"${tweet.entities.get.media.head.media_url}")
+
+        println(s"${tweet.id}, ${tweet.user.get.name}, @${tweet.user.get.screen_name}, ${tweet.text}, ${imageGet.mkString(",")}")
+
       }
       case Failure(t) => println(t.getMessage())
     }
