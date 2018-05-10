ce5ac8350cd45b46b4bb6334819797d51479c5da
astel <kiyomillefeuille@gmail.com>
2018-05-10 17:31:50 +0900

    画像をローカルに保存

--- src/main/scala/net/astail/crawler/SearchTweet.scala
+++ src/main/scala/net/astail/crawler/SearchTweet.scala
@@ -2,17 +2,19 @@ package net.astail.crawler
 
 import com.danielasfregola.twitter4s.TwitterRestClient
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success}
 import scala.concurrent.ExecutionContext.Implicits.global
-
+import sys.process.urlToProcess
+import java.net.URL
+import java.io.File
 
 object SearchTweet {
 
   def search = {
     val client = TwitterRestClient()
-    val searchTw = client.searchTweet("モンエナうぐぅ", count = 10)
+    val searchTw = client.searchTweet("HHKB lang:ja", count = 500)
 
     searchTw.onComplete {
       case Success(msg) => for (tweet <- msg.data.statuses) {
@@ -22,11 +24,21 @@ object SearchTweet {
         }
 
         println(s"${tweet.id}, ${tweet.user.get.name}, @${tweet.user.get.screen_name}, ${tweet.text}, ${images.map(_.mkString(",")).getOrElse("NoData")}")
+
+        images match  {
+          case Some(x) => for(yy <- x) download(yy)
+          case None => println("noImage")
+        }
       }
       case Failure(t) => println(t.getMessage())
     }
 
     Await.ready(searchTw, Duration.Inf)
     client.shutdown()
+  }
+
+  def download(url: String): Future[String] = Future {
+    val name = url.split("/").reverse.toList.headOption.get
+    new URL(url) #> new File(s"./get_files/${name}") !!
   }
 }
